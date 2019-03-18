d0f0901c0f0db076108b2258d80aec25d8b0a82f
Alexander Polovnikov <alexpol25@yandex.ru>
2019-03-18 23:27:11 +0300

    remove logic from bin file

--- src/even-game.js
+++ src/even-game.js
@@ -1,18 +1,42 @@
 import readlineSync from 'readline-sync';
 
-export const generateNumber = () => Math.ceil(Math.random() * 100);
+const GAME_ROUNDS = 3;
+let gameRound = 0;
 
-export const isEven = n => n % 2 === 0;
+const generateNumber = () => Math.ceil(Math.random() * 100);
 
-export const askAnswer = () => readlineSync.question('Your answer: ');
+const isEven = n => n % 2 === 0;
 
-export const checkAnswer = (number, answer) => ((isEven(number) && answer === 'yes') || (!isEven(number) && answer === 'no'));
+const askAnswer = () => readlineSync.question('Your answer: ');
 
-export const showQuestion = (number) => {
+const checkAnswer = (number, answer) => ((isEven(number) && answer === 'yes') || (!isEven(number) && answer === 'no'));
+
+const showQuestion = (number) => {
   console.log(`Question: ${number}`);
 };
 
-export const showError = (answer, number, name) => {
+const showError = (answer, number, name) => {
   const rightAnswer = isEven(number) ? "'yes'" : "'no'";
   console.log(`'${answer}' is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${name}!`);
 };
+
+const startGame = (name) => {
+  while (gameRound < GAME_ROUNDS) {
+    const number = generateNumber();
+    showQuestion(number);
+    const answer = askAnswer();
+    const isTrueAnswer = checkAnswer(number, answer);
+    if (isTrueAnswer) {
+      gameRound += 1;
+      console.log('Correct!');
+      if (gameRound === GAME_ROUNDS) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    } else {
+      showError(answer, number, name);
+      break;
+    }
+  }
+};
+
+export default startGame;
