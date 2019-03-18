cbe6601e3a98d3758b179581536ad2885340333f
Alexander Polovnikov <alexpol25@yandex.ru>
2019-03-18 14:59:28 +0300

    add ask name functional

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+export function showGreeting() {
+    console.log('Welcome to the Brain Games!\n')
+}
+
+export function askName() {
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+};
+
