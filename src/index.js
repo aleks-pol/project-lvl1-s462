6c700b67275bf5965c82610359612cd8fea6d9a1
Alexander Polovnikov <alexpol25@yandex.ru>
2019-03-18 17:03:49 +0300

    fix feedback

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export function showGreeting() {
+export const showGreeting = () => {
   console.log('Welcome to the Brain Games!\n');
-}
+};
 
-export function askName() {
+export const askName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-}
+};
