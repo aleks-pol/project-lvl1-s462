f0fc0f4bc2b4dfc9c4b144dcd2485714346aa4fb
Alexander Polovnikov <alexpol25@yandex.ru>
2019-03-18 15:52:12 +0300

    #Task3, add linter and CI

---
+++ README.md
@@ -0,0 +1,15 @@
+# Brain Games
+[![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/aleks-pol/project-lvl1-s462/maintainability)
+[![Build Status](https://travis-ci.org/aleks-pol/project-lvl1-s462.svg?branch=master)](https://travis-ci.org/aleks-pol/project-lvl1-s462)
+
+## Setup
+
+```sh
+$ make install
+```
+
+## Run
+
+```sh
+$ make start
+```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import readlineSync from 'readline-sync';
 
 export function showGreeting() {
-    console.log('Welcome to the Brain Games!\n')
+  console.log('Welcome to the Brain Games!\n');
 }
 
 export function askName() {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 }
-
-
